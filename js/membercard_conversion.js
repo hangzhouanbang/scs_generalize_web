49a8950cca4e2bed2653f44e0e5fdab9cd6808c7
cwj <1755140651@qq.com>
2018-07-16 14:48:42 +0800

    兑换、购买弹窗

--- js/membercard_conversion.js
+++ js/membercard_conversion.js
@@ -1,3 +1,17 @@
-function memberCardConversion() {
-    window.location.href = "../html/membercard_conversion.html";
+function show(dom1, dom2) {
+    for (var i = 0; i < dom1.length; i++) {
+        dom1[i].style.display = 'block';
+    }
+    ;
+    for (var j = 0; j < dom1.length; j++) {
+        dom2[j].style.display = 'none';
+    }
+    ;
+}
+
+function hide(dom) {
+    for (var i = 0; i < dom.length; i++) {
+        dom[i].style.display = 'none';
+    }
+    ;
 }
