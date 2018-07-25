da49a301100229d731bea8bde04fced3f64f4c89
cwj <1755140651@qq.com>
2018-07-25 13:51:44 +0800

    积分兑换会员卡联调

--- js/membercard_buy.js
+++ js/membercard_buy.js
@@ -18,59 +18,58 @@ function hide(dom) {
     init();
 }
 
-var token = '28757783-0d14-4c28-963b-daf2d9a0b10d'
+var token = 'f82fdb93-7ea1-46e1-aeab-09a82b5cab30'
 
 //初始化数据
 function init() {
     ajax_method(map.localurl + map.queryagentclubcard, 'token=' + token + '&payType=微信', 'post', function successCallBack(a) {
         var data = JSON.parse(a)
-        html = ' 购买物品\n' +
-            '    <tr>\n' +
-            '        <td align="center">\n' +
-            '            <div class="d_goods">\n' +
-            '                <div><img src="data.data.items[0].productPic" style="width: 1rem;height: 1rem;" class="i_img"></div>\n' +
-            '                <span>' + data.data.items[0].number + '张</span>\n' +
-            '                <span>' + data.data.items[0].price + '元</span>\n' +
-            '                <div class="btn"\n' +
-            '                     onclick="show(document.getElementsByClassName(\'Donation\'),document.getElementsByClassName(\'confirm_prepaid_phone\'))">\n' +
-            '                    购买\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '        </td>\n' +
-            '        <td align="center">\n' +
-            '            <div class="d_goods">\n' +
-            '                <div><img src="data.data.items[1].productPic" style="width: 1rem;height: 1rem;" class="i_img"></div>\n' +
-            '                <span>' + data.data.items[1].number + '张</span>\n' +
-            '                <span>' + data.data.items[1].price + '元</span>\n' +
-            '                <div class="btn"\n' +
-            '                     onclick="show(document.getElementsByClassName(\'Donation\'),document.getElementsByClassName(\'confirm_prepaid_phone\'))">\n' +
-            '                    购买\n' +
-            '                </div>\n' +
-            '            </div>\n' +
-            '        </td>\n' +
-            '    </tr>\n';
-        document.getElementById('table_buy').innerHTML = html;
+        for (var i = 0; i < data.data.items.length; i++) {
+            var tr = document.createElement('tr');
+            tr.innerHTML =
+                '        <td align="center"rowspan="3">' +
+                '            <div class="d_goods">\n' +
+                '                <div><img src="' + data.data.items[i].productPic + '" style="width: 1rem;height: 1rem;" class="i_img"></div>\n' +
+                '                <span>' + data.data.items[i].number + '张</span>\n' +
+                '                <span>' + data.data.items[i].price + '元</span>\n' +
+                '                <div class="btn"\n' +
+                '                     onclick="show(document.getElementsByClassName(\'Donation\'),document.getElementsByClassName(\'confirm_prepaid_phone\'))">\n' +
+                '                    购买\n' +
+                '                </div>\n' +
+                '            </div>\n' +
+                '        </td>';
+            sessionStorage.setItem('id', data.data.items[i].id);
+            document.getElementById('table_buy').appendChild(tr);
 
-        html2 = ' <div class="headline">请确认购买内容<br>\n' +
-            '        以' + data.data.items[0].price + '元购买' + data.data.items[0].number + '张会员周卡\n' +
-            '    </div>\n' +
-            '    <div class="querycard">\n' +
-            '        <span onclick="show(document.getElementsByClassName(\'examples_of_successful\'),document.getElementsByClassName(\'Donation\'))">确认购买</span>\n' +
-            '        <a href="membercard_buy.html" onclick="hide(document.getElementsByClassName(\'Donation\'))">我再想想</a>\n' +
-            '    </div>';
-        document.getElementById('don').innerHTML = html2;
+            html2 = ' <div class="headline">请确认购买内容<br>\n' +
+                '        以' + data.data.items[i].price + '元购买' + data.data.items[i].number + '张会员周卡\n' +
+                '    </div>\n' +
+                '    <div class="querycard">\n' +
+                '        <span onclick="qr()">确认购买</span>\n' +
+                '        <a href="membercard_buy.html" onclick="hide(document.getElementsByClassName(\'Donation\'))">我再想想</a>\n' +
+                '    </div>';
+            document.getElementById('don').innerHTML = html2;
 
-        html3 = '<div class="headline">您成功购买<br/>会员周卡' + data.data.items[0].number + '张</div>\n' +
-            '    <div class="querycard">\n' +
-            '        <a href="purchase_history.html">查看购买记录</a>\n' +
-            '        <a href="first_index.html" onclick="hide(document.getElementsByClassName(\'examples_of_successful\'))">返回首页</a>\n' +
-            '    </div>\n' +
-            '    <br>\n' +
-            '    <div class="mess">目前会员周卡：6张，月卡：3张，季卡：3张</div>\n' +
-            '    <br>';
-        document.getElementById('eos').innerHTML = html3;
-
+            html3 = '<div class="headline">您成功购买<br/>会员周卡' + data.data.items[i].number + '张</div>\n' +
+                '    <div class="querycard">\n' +
+                '        <a href="purchase_history.html">查看购买记录</a>\n' +
+                '        <a href="first_index.html" onclick="hide(document.getElementsByClassName(\'examples_of_successful\'))">返回首页</a>\n' +
+                '    </div>\n' +
+                '    <br>\n' +
+                '    <div class="mess">目前会员周卡：6张，月卡：3张，季卡：3张</div>\n' +
+                '    <br>';
+            document.getElementById('eos').innerHTML = html3;
+        }
     })
 }
 
 init();
+
+function qr() {
+    ajax_method(map.localurl + map.buyscoreclubcard, 'token=' + token + '&cardId=' + sessionStorage.getItem('id'), 'post', function successCallBack(a) {
+        var sure = JSON.parse(a);
+        if (sure.success) {
+            show(document.getElementsByClassName('examples_of_successful'), document.getElementsByClassName('Donation'))
+        }
+    })
+}
--- js/membercard_conversion.js
+++ js/membercard_conversion.js
@@ -24,14 +24,12 @@ var token = 'f82fdb93-7ea1-46e1-aeab-09a82b5cab30'
 function init() {
     ajax_method(map.localurl + map.queryagentclubcard, 'token=' + token + '&payType=积分', 'post', function successCallBack(a) {
         var data = JSON.parse(a)
-
-
         for (var i = 0; i < data.data.items.length; i++) {
             var tr = document.createElement('tr');
             tr.innerHTML =
                 '        <td align="center" rowspan="2">\n' +
                 '            <div class="d_goods">\n' +
-                '                <div><img src="data.data.items[i].productPic" style="width: 1rem;height: 1rem;" class="i_img"></div>\n' +
+                '                <div><img src="' + data.data.items[i].productPic + '" style="width: 1rem;height: 1rem;" class="i_img"></div>\n' +
                 '                <span id="sp1">' + data.data.items[i].number + '张</span>\n' +
                 '                <span>' + data.data.items[i].price + '积分</span>\n' +
                 '                <div class="btn"\n' +
@@ -42,27 +40,26 @@ function init() {
                 '        </td>\n';
             sessionStorage.setItem('id', data.data.items[i].id);
             document.getElementById('table_integral').appendChild(tr);
+
+            html2 = ' <div class="headline">请确认兑换内容<br>\n' +
+                '        消耗' + data.data.items[i].price + '积分兑换' + data.data.items[i].number + '张月卡\n' +
+                '    </div>\n' +
+                '    <div class="querycard">\n' +
+                '        <span  onclick="qr()">确认兑换</span>\n' +
+                '        <a href="membercard_conversion.html" onclick="hide(document.getElementsByClassName(\'Donation\'))">我再想想</a>\n' +
+                '    </div>';
+            document.getElementById('don').innerHTML = html2;
+
+            html3 = ' <div class="headline">恭喜您成功兑换<br/>' + data.data.items[i].number + '张月卡</div>\n' +
+                '    <div class="querycard">\n' +
+                '        <a href="for_record.html">查看兑换记录</a>\n' +
+                '        <a href="first_index.html" onclick="hide(document.getElementsByClassName(\'examples_of_successful\'))">返回首页</a>\n' +
+                '    </div>\n' +
+                '    <br>\n' +
+                '    <div class="mess">目前会员周卡：6张，月卡：3张，季卡：3张</div>\n' +
+                '    <br>';
+            document.getElementById('eos').innerHTML = html3;
         }
-
-        html2 = ' <div class="headline">请确认兑换内容<br>\n' +
-            '        消耗' + data.data.items[0].price + '积分兑换' + data.data.items[0].number + '张月卡\n' +
-            '    </div>\n' +
-            '    <div class="querycard">\n' +
-            '        <span  onclick="qr()">确认兑换</span>\n' +
-            '        <a href="membercard_conversion.html" onclick="hide(document.getElementsByClassName(\'Donation\'))">我再想想</a>\n' +
-            '    </div>';
-        document.getElementById('don').innerHTML = html2;
-
-        html3 = ' <div class="headline">恭喜您成功兑换<br/>' + data.data.items[i].number + '张月卡</div>\n' +
-            '    <div class="querycard">\n' +
-            '        <a href="for_record.html">查看兑换记录</a>\n' +
-            '        <a href="first_index.html" onclick="hide(document.getElementsByClassName(\'examples_of_successful\'))">返回首页</a>\n' +
-            '    </div>\n' +
-            '    <br>\n' +
-            '    <div class="mess">目前会员周卡：6张，月卡：3张，季卡：3张</div>\n' +
-            '    <br>';
-        document.getElementById('eos').innerHTML = html3;
-
     })
 }
 
